9839d902a630d52e5679937a963c423d35ec62c4
OS-ricardomoreirasilva <ricardo.moreira.silva@outsystems.com>
2024-06-25 17:11:52 +0100

    chore(ios): add ANALYTICS_COLLECTION_ENABLED to info.plist
    Aligned with our interpretation of Firebase's recommendations, this property is just added when it's explicitly set to false.
    Change the "onload" property to yes, as plugin variables aren't an option with OutSystems.
    
    References: https://outsystemsrd.atlassian.net/browse/RMET-3503

--- hooks/ios/iOSCopyPreferences.js
+++ hooks/ios/iOSCopyPreferences.js
@@ -7,14 +7,12 @@ module.exports = function (context) {
     var projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
     var configXML = path.join(projectRoot, 'config.xml');
     var configParser = new ConfigParser(configXML);
-    var enableAppTracking = configParser.getPlatformPreference("EnableAppTrackingTransparencyPrompt", "ios");
-
-    var appNamePath = path.join(projectRoot, 'config.xml');
-    var appNameParser = new ConfigParser(appNamePath);
-    var appName = appNameParser.name();
+    
+    var appName = configParser.name();
     var infoPlistPath = path.join(projectRoot, 'platforms/ios/' + appName + '/'+ appName +'-info.plist');
     var obj = plist.parse(fs.readFileSync(infoPlistPath, 'utf8'));
 
+    var enableAppTracking = configParser.getPlatformPreference("EnableAppTrackingTransparencyPrompt", "ios");
     if(enableAppTracking == "true" || enableAppTracking == ""){
         var userTrackingDescription = configParser.getPlatformPreference("USER_TRACKING_DESCRIPTION_IOS", "ios");
         if(userTrackingDescription != ""){
@@ -26,4 +24,11 @@ module.exports = function (context) {
         delete obj['NSUserTrackingUsageDescription'];
         fs.writeFileSync(infoPlistPath, plist.build(obj));
     }
+
+    var collectionEnabled = configParser.getGlobalPreference("ANALYTICS_COLLECTION_ENABLED");
+    if (collectionEnabled.toLowerCase() == 'false') {
+        obj['FIREBASE_ANALYTICS_COLLECTION_ENABLED'] = false;
+    } 
+
+    fs.writeFileSync(infoPlistPath, plist.build(obj));
 };
