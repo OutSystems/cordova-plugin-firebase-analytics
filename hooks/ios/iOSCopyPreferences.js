5a1da7dd91c2a6861b19e94c2a46263d492d6e29
OS-ricardomoreirasilva <ricardo.moreira.silva@outsystems.com>
2024-06-28 10:07:32 +0100

    chore(ios): include pr comments
    Replace 'var' with 'let'.
    
    References: https://outsystemsrd.atlassian.net/browse/RMET-3503

--- hooks/ios/iOSCopyPreferences.js
+++ hooks/ios/iOSCopyPreferences.js
@@ -4,17 +4,17 @@ const plist = require('plist');
 const { ConfigParser } = require('cordova-common');
 
 module.exports = function (context) {
-    var projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
-    var configXML = path.join(projectRoot, 'config.xml');
-    var configParser = new ConfigParser(configXML);
+    let projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
+    let configXML = path.join(projectRoot, 'config.xml');
+    let configParser = new ConfigParser(configXML);
     
-    var appName = configParser.name();
-    var infoPlistPath = path.join(projectRoot, 'platforms/ios/' + appName + '/'+ appName +'-info.plist');
-    var obj = plist.parse(fs.readFileSync(infoPlistPath, 'utf8'));
+    let appName = configParser.name();
+    let infoPlistPath = path.join(projectRoot, 'platforms/ios/' + appName + '/'+ appName +'-info.plist');
+    let obj = plist.parse(fs.readFileSync(infoPlistPath, 'utf8'));
 
-    var enableAppTracking = configParser.getPlatformPreference("EnableAppTrackingTransparencyPrompt", "ios");
+    let enableAppTracking = configParser.getPlatformPreference("EnableAppTrackingTransparencyPrompt", "ios");
     if(enableAppTracking == "true" || enableAppTracking == ""){
-        var userTrackingDescription = configParser.getPlatformPreference("USER_TRACKING_DESCRIPTION_IOS", "ios");
+        let userTrackingDescription = configParser.getPlatformPreference("USER_TRACKING_DESCRIPTION_IOS", "ios");
         if(userTrackingDescription != ""){
             obj['NSUserTrackingUsageDescription'] = userTrackingDescription;
             fs.writeFileSync(infoPlistPath, plist.build(obj));
@@ -25,7 +25,7 @@ module.exports = function (context) {
         fs.writeFileSync(infoPlistPath, plist.build(obj));
     }
 
-    var collectionEnabled = configParser.getGlobalPreference("ANALYTICS_COLLECTION_ENABLED");
+    let collectionEnabled = configParser.getGlobalPreference("ANALYTICS_COLLECTION_ENABLED");
     if (collectionEnabled.toLowerCase() == 'false') {
         obj['FIREBASE_ANALYTICS_COLLECTION_ENABLED'] = false;
     } 
