906f6e8be3a499a00ee1c5d2247866bfbdf06177
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2024-10-07 14:20:55 -0400

    chore: enums instead of helper func

--- src/ios/Common/OSFANLConsentHelper.swift
+++ src/ios/Common/OSFANLConsentHelper.swift
@@ -1,8 +1,35 @@
 import FirebaseAnalytics
 import FirebaseCore
 
-@objc class OSFANLConsentHelper: NSObject {
+@objc enum ConsentTypeRawValue: Int {
+    case adPersonalization = 1
+    case adStorage = 2
+    case adUserData = 3
+    case analyticsStorage = 4
     
+    var stringValue: String {
+        switch self {
+        case .adPersonalization: return "ad_personalization"
+        case .adStorage: return "ad_storage"
+        case .adUserData: return "ad_user_data"
+        case .analyticsStorage: return "analytics_storage"
+        }
+    }
+}
+
+@objc enum ConsentStatusRawValue: Int {
+    case granted = 1
+    case denied = 2
+    
+    var stringValue: String {
+        switch self {
+        case .granted: return "granted"
+        case .denied: return "denied"
+        }
+    }
+}
+
+@objc class OSFANLConsentHelper: NSObject {
     @objc static func createConsentModel(_ commandArguments: NSArray) throws -> NSDictionary {
         guard let jsonString = commandArguments[0] as? String,
               let jsonData = jsonString.data(using: .utf8),
@@ -13,20 +40,20 @@ import FirebaseCore
         var firebaseConsentDict: [ConsentType: ConsentStatus] = [:]
         
         for item in array {
-            guard let type = item["Type"] as? NSNumber,
-                  let status = item["Status"] as? NSNumber else {
-                throw OSFANLError.invalidType("Type or Status", type: "NSNumber")
+            guard let typeRawValue = item["Type"] as? Int,
+                  let statusRawValue = item["Status"] as? Int,
+                  let consentTypeRawValue = ConsentTypeRawValue(rawValue: typeRawValue),
+                  let consentStatusRawValue = ConsentStatusRawValue(rawValue: statusRawValue) else {
+                throw OSFANLError.invalidType("Consent Type or Status", type: "Valid value")
             }
             
-            if let consentType = consentTypeFromNumber(type),
-               let consentStatus = consentStatusFromNumber(status) {
-                if firebaseConsentDict[consentType] != nil {
-                    throw OSFANLError.duplicateItemsIn(parameter: "ConsentSettings")
-                } else {
-                    firebaseConsentDict[consentType] = consentStatus
-                }
+            let consentType = ConsentType(rawValue: consentTypeRawValue.stringValue)
+            let consentStatus = ConsentStatus(rawValue: consentStatusRawValue.stringValue)
+            
+            if firebaseConsentDict[consentType] != nil {
+                throw OSFANLError.duplicateItemsIn(parameter: "ConsentSettings")
             } else {
-                throw OSFANLError.invalidType("Consent Type or Status", type: "Valid value")
+                firebaseConsentDict[consentType] = consentStatus
             }
         }
         
@@ -34,24 +61,6 @@ import FirebaseCore
             throw OSFANLError.missing("ConsentSettings")
         } else {
             return firebaseConsentDict as NSDictionary
-        }
-    }
-    
-    private static func consentTypeFromNumber(_ num: NSNumber) -> ConsentType? {
-        switch num.intValue {
-        case 1: return .adPersonalization
-        case 2: return .adStorage
-        case 3: return .adUserData
-        case 4: return .analyticsStorage
-        default: return nil
-        }
-    }
-
-    private static func consentStatusFromNumber(_ num: NSNumber) -> ConsentStatus? {
-        switch num.intValue {
-        case 1: return .granted
-        case 2: return .denied
-        default: return nil
         }
     }
 }
